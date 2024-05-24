6b70123bef88507bf6a9d514a970f6996a91c76c
NataliaNiebla <tataniebla2005@gmail.com>
2024-05-24 08:27:27 -0600

    updated

--- 6_ejercicios_de_repaso_1a5/ejercicio_9.py
+++
@@ -1,9 +0,0 @@
-"""
-9.- Hacer un programa que solicite numeros indefinidamente hasta que se introduzca el 111 y salir del programa
-"""
-
-while True:
-    x=int(input("Ingresa un número: "))
-    if x==111:
-        print("Número correcto")
-        break        
